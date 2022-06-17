d72e3deba900e742427435cfffa01e6d5cd1c933
rajish <maharjanrajish101@gmail.com>
2022-06-17 21:42:22 +0545

    product and category

--- Store/views.py
+++ Store/views.py
@@ -5,5 +5,18 @@ from .models import *
 
 # Create your views here.
 def Index(request):
-    product = Product.get_all_products()
-    return render(request,'index.html',{'product':product})
+    data = {}
+    products = None
+    categories = Category.get_all_categories()
+    # read string from url/server inthis case /?category = categoryId
+    # read data that is passed to server
+    categoryID = request.GET.get('category')
+    print(categoryID)
+    if categoryID:
+        products = Product.get_all_products_by_categoryid(categoryID)
+    else:
+        products = Product.get_all_products()
+
+    data['products'] = products
+    data['categories'] = categories
+    return render(request,'index.html',data)
