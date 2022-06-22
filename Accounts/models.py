673aa8b3ae52dcb346e5e62cc61a50403615f792
rajish <maharjanrajish101@gmail.com>
2022-06-22 21:03:33 +0545

    static method added

--- Accounts/models.py
+++ Accounts/models.py
@@ -12,6 +12,13 @@ class Customer(models.Model):
     def register(self):
         self.save()
 
+    @staticmethod
+    def get_customer_by_email(email):
+        try:
+            return Customer.objects.get(email = email)
+        except:
+            return False
+
     # check if user exists
     def isExists(self):
         if Customer.objects.filter(email = self.email):
