c269268c01005225a801f94fe68bac4cd55edbb8
rajish <maharjanrajish101@gmail.com>
2022-06-22 16:49:58 +0545

    CustomerAdmin added

--- Accounts/admin.py
+++ Accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from Accounts.models import *
 
 # Register your models here.
+
+@admin.register(Customer)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('first_name','last_name','phone','email','password')
+
