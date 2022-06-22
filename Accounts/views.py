d93113e733c847bfaa251833eae7639afb5c4a84
rajish <maharjanrajish101@gmail.com>
2022-06-22 16:50:46 +0545

    Signup view

--- Accounts/views.py
+++ Accounts/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomerForm
+from django.contrib import messages
 
 # Create your views here.
 
 def SignUp(request):
-    return render(request,'signup.html')
+    context = {}
+    if request.method == "POST":
+        form = CustomerForm(request.POST)
+        # In the 'form' class the clean function is defined, if all the data is correct,as per the clean function, it returns true
+        if form.is_valid(): 
+            first_name= form.cleaned_data.get('username')
+            form.save()
+            messages.success(request,f"Account for {first_name} successfully created.")
+            return redirect('store:index')
+    else:
+
+        form = CustomerForm()
+    context['form'] = form
+    return render(request,'signup.html',context)
