e110e60f2e80ed53d72702ba191138ca42ff94b7
rajish <maharjanrajish101@gmail.com>
2022-06-22 17:00:04 +0545

    Secretkey hidden

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6x+l9=8icu9!67l*s+o0(+n1#r$v5l0bn3rtmtfwqp9h!q4hd*'
-
+from decouple import config
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
