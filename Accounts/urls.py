3cfe0ab52ddb530f82b4b4b0512815b08829df89
rajish <maharjanrajish101@gmail.com>
2022-06-22 21:03:57 +0545

    login url added

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "account"
 
 urlpatterns = [
-    path('signup/',views.SignUp, name = 'signup')
+    path('signup/',views.SignUp, name = 'signup'),
+    path('login/',views.Login, name = 'login'),
 ]
