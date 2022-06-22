c3c7a932edb1ac65e19c1e87cc180eac11b90b33
rajish <maharjanrajish101@gmail.com>
2022-06-22 16:50:20 +0545

    few changes

--- Accounts/models.py
+++ Accounts/models.py
@@ -5,6 +5,15 @@ from django.db import models
 class Customer(models.Model):
     first_name = models.CharField(max_length=25)
     last_name = models.CharField(max_length=30)
-    phone = models.CharField(max_length=10)
-    email = models.EmailField()
+    phone = models.CharField(max_length=14)
+    email = models.EmailField(unique=True)
     password = models.CharField(max_length=20)
+
+    def register(self):
+        self.save()
+
+    # check if user exists
+    def isExists(self):
+        if Customer.objects.filter(email = self.email):
+            return True
+        return False
