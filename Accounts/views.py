e8b099fa78d92a17a97284e6fc85a3ab9d19360b
rajish <maharjanrajish101@gmail.com>
2022-06-27 11:54:33 +0545

    class based view

--- Accounts/views.py
+++ Accounts/views.py
@@ -3,12 +3,18 @@ from .forms import CustomerForm
 from django.contrib import messages
 from .models import Customer
 from django.contrib.auth.hashers import check_password
+from django.views import View
 
 # Create your views here.
 
-def SignUp(request):
-    context = {}
-    if request.method == "POST":
+class SignUp(View):
+
+    def get(self,request):
+        form = CustomerForm()
+        return render(request,'signup.html',{'form':form})
+        
+
+    def post(self,request):
         form = CustomerForm(request.POST)
         # In the 'form' class the clean function is defined, if all the data is correct,as per the clean function, it returns true
         if form.is_valid(): 
@@ -16,14 +22,13 @@ def SignUp(request):
             form.save()
             messages.success(request,f"Account for {first_name} successfully created.")
             return redirect('store:index')
-    else:
-        form = CustomerForm()
-    context['form'] = form
-    return render(request,'signup.html',context)
 
-def Login(request):
-    # context = {}
-    if request.method == "POST":
+class Login(View):
+
+    def get(self,request):
+        return render(request,'login.html')
+
+    def post(self,request):
         email = request.POST.get('email')
         # get password from login form
         password = request.POST.get('password')
@@ -42,6 +47,4 @@ def Login(request):
                 messages.info(request,f"Email or Password Invalid !")
         else:
             messages.info(request,f"Account not found with given email.")
-    else:
-        return render(request,'login.html')
-    return render(request,'login.html')
+
