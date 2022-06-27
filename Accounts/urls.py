a46d88c80d94b804129ec6cb583cbe09bb08cf32
rajish <maharjanrajish101@gmail.com>
2022-06-27 11:55:04 +0545

    updated for CBV

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from .views import SignUp,Login
 
 app_name = "account"
 
 urlpatterns = [
-    path('signup/',views.SignUp, name = 'signup'),
-    path('login/',views.Login, name = 'login'),
+    path('signup/',SignUp.as_view(), name = 'signup'),
+    path('login/',Login.as_view(), name = 'login'),
 ]
