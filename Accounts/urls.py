a13ce0cbf9aa551f1c7f417dac9f69a831177ea4
rajish <maharjanrajish101@gmail.com>
2022-06-27 12:10:23 +0545

    url mapped

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
-from .views import SignUp,Login
+from .views import signup,login
 
 app_name = "account"
 
 urlpatterns = [
-    path('signup/',SignUp.as_view(), name = 'signup'),
-    path('login/',Login.as_view(), name = 'login'),
+    path('signup/',signup.SignUp.as_view(), name = 'signup'),
+    path('login/',login.Login.as_view(), name = 'login'),
 ]
