384b6b09316b743f4488cd26824d05782d20c401
rajish <maharjanrajish101@gmail.com>
2022-06-29 21:23:27 +0545

    filter to check cart quantity

--- Store/templatetags/cart.py
+++ Store/templatetags/cart.py
@@ -11,3 +11,12 @@ def is_in_cart(product,cart):
             # if product already added in cart
             return True
     return False
+
+@register.filter(name = 'cart_quantity')
+def cart_quantity(product,cart):
+    keys = cart.keys()
+    for id in keys:
+        if int(id) == product.id:
+            # returns value i.e, quantity
+            return cart.get(id)
+    return 0;
