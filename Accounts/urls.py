7f8e4d939e4661953a32e3180b97e30c0bad4401
rajish <maharjanrajish101@gmail.com>
2022-06-30 15:38:28 +0545

    logout endpoint

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .views import signup,login
+from .views import signup,login,logout
 
 app_name = "account"
 
 urlpatterns = [
     path('signup/',signup.SignUp.as_view(), name = 'signup'),
     path('login/',login.Login.as_view(), name = 'login'),
+    path('logout/',logout.logout,name = 'logout'),
 ]
