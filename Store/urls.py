b1b7d508092b17bbc5cfbe71fbcaeafd80f276d8
rajish <maharjanrajish101@gmail.com>
2022-06-30 21:46:27 +0545

    cart endpoint added

--- Store/urls.py
+++ Store/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index,cart
 
 app_name = 'store'
 
 urlpatterns = [
-    path('',index.IndexView.as_view(), name = 'index')
+    path('',index.IndexView.as_view(), name = 'index'),
+    path('cart',cart.Cart.as_view(),name = 'cart')
 ]
