99d59e7aacf75408f620066d8016a52b81c0f6d0
rajish <maharjanrajish101@gmail.com>
2022-07-04 22:08:28 +0545

    Order urls included

--- Eshop/urls.py
+++ Eshop/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('Store.urls')),
-    path('accounts/',include('Accounts.urls'))
+    path('accounts/',include('Accounts.urls')),
+    path('order/',include('Orders.urls')),
 ]
 
 
