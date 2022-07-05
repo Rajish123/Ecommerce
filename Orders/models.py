450809598d0fd786f64ddada2ff62cfc61031bc4
rajish <maharjanrajish101@gmail.com>
2022-07-05 15:13:34 +0545

    static method added

--- Orders/models.py
+++ Orders/models.py
@@ -12,6 +12,11 @@ class Order(models.Model):
     address = models.CharField(max_length=50, default='',blank=True)
     phone= models.CharField(max_length=50, default='',blank=True)
     date = models.DateField(default=datetime.today)
+    status = models.BooleanField(default=False)
 
     def placeOrder(self):
         self.save()
+
+    @staticmethod
+    def get_orders_by_customer(customer_id):
+        return Order.objects.filter(customer = customer_id).order_by('-date')
