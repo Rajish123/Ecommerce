5e5a21ffcfda3822575a842df2a4e0308ab4b6d5
rajish <maharjanrajish101@gmail.com>
2022-07-05 16:37:35 +0545

    middleware added

--- Orders/urls.py
+++ Orders/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import checkout, orders
+from Store.middlewares.auth import auth_middleware
 
 app_name = 'order'
 
 urlpatterns = [
     path('checkout',checkout.CheckOut.as_view(),name = 'checkout'),
-    path('orders',orders.OrderView.as_view(),name = 'orders')
+    path('orders',auth_middleware(orders.OrderView.as_view()),name = 'orders')
 ]
 
