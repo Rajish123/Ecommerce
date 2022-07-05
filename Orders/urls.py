cc8179a6cd100024741911bc38ebb58969177bf0
rajish <maharjanrajish101@gmail.com>
2022-07-05 15:13:53 +0545

    order url added

--- Orders/urls.py
+++ Orders/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import checkout
+from .views import checkout, orders
 
 app_name = 'order'
 
 urlpatterns = [
-    path('checkout',checkout.CheckOut.as_view(),name = 'checkout')
+    path('checkout',checkout.CheckOut.as_view(),name = 'checkout'),
+    path('orders',orders.OrderView.as_view(),name = 'orders')
 ]
 
