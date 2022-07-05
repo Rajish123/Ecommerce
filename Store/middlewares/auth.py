146965263c0f0d38e808d790178d68da309946cb
rajish <maharjanrajish101@gmail.com>
2022-07-05 17:14:28 +0545

    return url added in query string

--- Store/middlewares/auth.py
+++ Store/middlewares/auth.py
@@ -5,8 +5,18 @@ from django.shortcuts import redirect
 def auth_middleware(get_response):
 
     def middleware(request):
+        # from which url the request is coming
+        # returns a url, which you havve visited.In this case you cannot visit order page without login,
+        # if you visit order page by logging in and clear the cookies, and again tried to visit 
+        # order page you are redirected to login page.
+        # PATH_INFO shows from which url you are redirected. Here url is orders/order
+
+        returnUrl = request.META['PATH_INFO']
+        # print(request.META['PATH_INFO'])
+        
         if not request.session.get('customer'):
-            return redirect('account:login')
+            # now if you login you will be directly redircted to orders page
+            return redirect(f'/accounts/login?return_url = {returnUrl}')
         response = get_response (request)
         return response
     return middleware
