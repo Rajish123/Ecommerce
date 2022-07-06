b39799dcc853ce1e67e261d73e4fab82e230392c
rajish <maharjanrajish101@gmail.com>
2022-07-06 15:03:27 +0545

    return url updated

--- Orders/views/checkout.py
+++ Orders/views/checkout.py
@@ -1,3 +1,4 @@
+from itertools import product
 from urllib import request
 from Orders.models import Order
 from django.views import View
@@ -5,8 +6,17 @@ from django.shortcuts import redirect, render
 from Store.models import Product
 from Accounts.models import Customer
 
-# mport urllib.request
 class CheckOut(View):
+    def get(self,request):
+        customer = request.session.get('customer')
+        # cart = {'product':quantity}, get quantity from cart object
+        cart = request.session.get('cart')
+        print(cart.keys())
+        # get all product ids
+        products = Product.get_product_by_id(list(request.session.get('cart').keys()))
+        return render(request,'cart.html',{'products':products})        
+
+
     def post(self,request):
         address = request.POST.get('address')
         phone = request.POST.get('phone')
