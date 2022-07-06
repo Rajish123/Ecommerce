f729a0d7a5ef575f76b2ae7c09620533ad42c345
rajish <maharjanrajish101@gmail.com>
2022-07-06 15:00:48 +0545

    login url update

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -6,6 +6,6 @@ app_name = "account"
 
 urlpatterns = [
     path('signup',signup.SignUp.as_view(), name = 'signup'),
-    path('login',login.Login.as_view(), name = 'login'),
+    path('login/',login.Login.as_view(), name = 'login'),
     path('logout',logout.logout,name = 'logout'),
 ]
