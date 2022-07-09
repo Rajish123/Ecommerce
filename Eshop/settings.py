dfccf80e830a8f9446ef69a170c363a750e41ac2
rajish <maharjanrajish101@gmail.com>
2022-07-09 19:43:46 +0545

    static url added

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -130,3 +130,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = 'media/'
 MEDIA_URL = os.path.join(BASE_DIR, 'media/')
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
