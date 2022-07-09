ea81bb08a399c9595332e4801b870a0011082252
rajish <maharjanrajish101@gmail.com>
2022-07-09 19:47:06 +0545

    view for cart

--- Store/views/cart.py
+++ Store/views/cart.py
@@ -7,5 +7,6 @@ class Cart(View):
     def get(self,request):
         # retrieve list of products or product id you added in cart from cart session
         ids = list(request.session.get('cart').keys())
+        print(ids)
         products = Product.get_product_by_id(ids)
         return render(request,'cart.html',{'products':products})
