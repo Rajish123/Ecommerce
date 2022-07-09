7a9730cc1fcd822c2b7b62bde1566e16f0f0a7a7
rajish <maharjanrajish101@gmail.com>
2022-07-09 19:44:25 +0545

    static method added for get_product

--- Store/models.py
+++ Store/models.py
@@ -29,6 +29,10 @@ class Product(models.Model):
     updated = models.DateTimeField(auto_now=True)
 
     @staticmethod
+    def get_product(id):
+        return Product.objects.get(id = id)
+
+    @staticmethod
     # here ids is list of product id
     def get_product_by_id(ids):
         return Product.objects.filter(id__in = ids)
