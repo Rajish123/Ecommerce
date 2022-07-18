97703a316bb191c167bef12826a8df1dd491472c
rajish <maharjanrajish101@gmail.com>
2022-07-18 20:33:58 +0545

    payment view added

--- Orders/views/checkout.py
+++ Orders/views/checkout.py
@@ -1,10 +1,66 @@
+from importlib import resources
 from itertools import product
 from urllib import request
 from Orders.models import Order
 from django.views import View
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, HttpResponse
 from Store.models import Product
 from Accounts.models import Customer
+from django.conf import settings
+import braintree
+
+
+def total_payment(request):
+    sum = 0
+    cart = request.session.get('cart')
+    products = Product.get_product_by_id(list(request.session.get('cart').keys()))
+    for i in products:
+        sum += i.price
+    return sum
+
+
+def checkout_page(request):
+    # generate all other required data that you may need on the checkout page and add them to context
+    # if settings.BRAINTREE_PRODUCTION:
+    #     braintree_env = braintree.Environment.Production
+    # else:
+    braintree_env = braintree.Environment.Sandbox
+
+    # configure braintree
+    braintree.Configuration.configure(
+        braintree_env,
+        merchant_id = settings.BRAINTREE_MERCHANT_ID,
+        public_key = settings.BRAINTREE_PUBLIC_KEY,
+        private_key = settings.BRAINTREE_PRIVATE_KEY,
+    )
+    try:
+        braintree_client_token = braintree.ClientToken.generate({"customer_id":Customer.id})
+    except:
+        braintree_client_token = braintree.ClientToken.generate({})
+    context = {'braintree_client_token':braintree_client_token}
+    return render(request,'checkout.html',context)
+
+
+def payment(request):
+    nonce_from_the_client = request.POST['paymentMethodNonce']
+    customer_kwargs = {
+        "first_name": request.user.first_name,
+        "last_name": request.user.last_name,
+        "email": request.user.email,
+    }
+    customer_create = braintree.Customer.create(customer_kwargs)
+    customer_id = customer_create.customer.id
+    result = braintree.Transaction.sale({
+        "amount": total_payment(),
+        "payment_method_nonce": nonce_from_the_client,
+        "options": {
+            "submit_for_settlement": True
+        }
+    })
+
+    print(result)
+    return HttpResponse('Ok')
+
 
 class CheckOut(View):
     def get(self,request):
