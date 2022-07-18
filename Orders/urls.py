8dcf1da69db9720ce9b3cfb64170bb6798af609d
rajish <maharjanrajish101@gmail.com>
2022-07-18 20:33:29 +0545

    route for checkout

--- Orders/urls.py
+++ Orders/urls.py
@@ -5,7 +5,11 @@ from Store.middlewares.auth import auth_middleware
 app_name = 'order'
 
 urlpatterns = [
-    path('checkout',auth_middleware(checkout.CheckOut.as_view()),name = 'checkout'),
-    path('orders',auth_middleware(orders.OrderView.as_view()),name = 'orders')
+    path('total',checkout.total_payment),
+    path('checkout',auth_middleware(checkout.checkout_page),name = 'checkout_page'),
+    
+    # path('checkout',auth_middleware(checkout.CheckOut.as_view()),name = 'checkout'),
+    path('orders',auth_middleware(orders.OrderView.as_view()),name = 'orders'),
+    path('payment',checkout.payment, name = 'payment')
 ]
 
