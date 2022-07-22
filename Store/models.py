4441827205cce868f6e12e74be8379f7e4722ea0
rajish <maharjanrajish101@gmail.com>
2022-07-22 18:28:09 +0545

    image resize

--- Store/models.py
+++ Store/models.py
@@ -52,8 +52,8 @@ class Product(models.Model):
     def save(self,*args,**kwargs):
         super().save(*args,**kwargs)
         img = Image.open(self.image.path)
-        if img.height > 200 or img.width > 200:
-            output_size = (150,150)
+        if img.height > 100 or img.width > 150:
+            output_size = (100,100)
             # resize previous image
             img.thumbnail(output_size)
             # override previous image
