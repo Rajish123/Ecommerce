a9c41f25eadc00ee8e54169a9d52e29a63097b18
rajish <maharjanrajish101@gmail.com>
2022-07-23 14:50:27 +0545

    endpoint for change and update profile

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import signup,login,logout
+from .views import signup,login,logout,profile,update_profile,change_password
 
 app_name = "account"
 
@@ -8,4 +8,7 @@ urlpatterns = [
     path('signup',signup.SignUp.as_view(), name = 'signup'),
     path('log',login.Login.as_view(), name = 'login'),
     path('logout',logout.logout,name = 'logout'),
+    path('profile',profile.Profile.as_view(),name='profile'),
+    path('update_profile',update_profile.UpdateProfile.as_view(),name='update_profile'),
+    path('change_password',change_password.ChangePassword.as_view(),name = 'change_password')
 ]
