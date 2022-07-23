e150c449edd5fb3ffccfdfa15bb8b606a5d98041
rajish <maharjanrajish101@gmail.com>
2022-07-23 14:51:08 +0545

    comments removed

--- Orders/views/checkout.py
+++ Orders/views/checkout.py
@@ -18,51 +18,6 @@ def total_payment(request):
         sum += i.price
     return sum
 
-
-# def checkout_page(request):
-#     # generate all other required data that you may need on the checkout page and add them to context
-#     # if settings.BRAINTREE_PRODUCTION:
-#     #     braintree_env = braintree.Environment.Production
-#     # else:
-#     braintree_env = braintree.Environment.Sandbox
-
-#     # configure braintree
-#     braintree.Configuration.configure(
-#         braintree_env,
-#         merchant_id = settings.BRAINTREE_MERCHANT_ID,
-#         public_key = settings.BRAINTREE_PUBLIC_KEY,
-#         private_key = settings.BRAINTREE_PRIVATE_KEY,
-#     )
-#     try:
-#         braintree_client_token = braintree.ClientToken.generate({"customer_id":Customer.id})
-#     except:
-#         braintree_client_token = braintree.ClientToken.generate({})
-#     context = {'braintree_client_token':braintree_client_token}
-#     return render(request,'checkout.html',context)
-
-
-# def payment(request):
-#     nonce_from_the_client = request.POST['paymentMethodNonce']
-#     customer_kwargs = {
-#         "first_name": request.user.first_name,
-#         "last_name": request.user.last_name,
-#         "email": request.user.email,
-#     }
-#     customer_create = braintree.Customer.create(customer_kwargs)
-#     customer_id = customer_create.customer.id
-#     # print(total_payment())
-#     result = braintree.Transaction.sale({
-#         "amount": total_payment(),
-#         "payment_method_nonce": nonce_from_the_client,
-#         "options": {
-#             "submit_for_settlement": True
-#         }
-#     })
-
-#     print(result)
-#     return HttpResponse('Ok')
-
-
 class CheckOut(View):
     def get(self,request):
         gateway = braintree.BraintreeGateway(settings.BRAINTREE_CONF)
