7a4cc75406e0d9f73526ddef451b7d5be8a7e607
rajish <maharjanrajish101@gmail.com>
2022-07-23 14:55:33 +0545

    few changes for templatetags

--- Store/templatetags/custom_filter.py
+++ Store/templatetags/custom_filter.py
@@ -10,3 +10,8 @@ def currency(number):
 @register.filter(name = "multiply")
 def multiply(price,quantity):
     return price * quantity
+
+@register.filter(name = 'cart_count')
+def cart_count(request):
+    cart = request.session.get('cart').keys()
+    return len(cart)   
