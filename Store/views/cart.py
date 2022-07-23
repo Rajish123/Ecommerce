01f2b6aaa24667c48b31bb3e40c32fac32476e0f
rajish <maharjanrajish101@gmail.com>
2022-07-23 14:52:54 +0545

    revert to previous changes

--- Store/views/cart.py
+++ Store/views/cart.py
@@ -6,8 +6,6 @@ from django.shortcuts import render
 class Cart(View):
     def get(self,request):
         # retrieve list of products or product id you added in cart from cart session
-        print(request.session.get('cart'))
         ids = list(request.session.get('cart').keys())
-        print(ids)
         products = Product.get_product_by_id(ids)
         return render(request,'cart.html',{'products':products})
