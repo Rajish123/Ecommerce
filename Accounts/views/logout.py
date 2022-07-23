356204931964d7bf4943398e8aef2c293b6946e9
rajish <maharjanrajish101@gmail.com>
2022-07-23 14:54:14 +0545

    few changes

--- Accounts/views/logout.py
+++ Accounts/views/logout.py
@@ -1,7 +1,13 @@
+from django.http import HttpResponseBadRequest
 from django.shortcuts import redirect
 from django.contrib import messages
  
 def logout(request):
-    request.session.clear()
-    messages.success(request,f"Logout Successful")
-    return redirect('store:index')
+    customer = request.session.get('customer')
+    if customer != None:
+        request.session.clear()
+        messages.success(request,f"Logout Successful")
+        return redirect('store:index')
+    else:
+        messages.error(request,"You are not logged in")
+        return redirect('store:index')
