45f45eae59df1e2c030317d6830d66f87cdd20d2
rajish <maharjanrajish101@gmail.com>
2022-07-26 20:01:07 +0545

    view for update profile

--- Accounts/views/update_profile.py
+++ Accounts/views/update_profile.py
@@ -17,18 +17,25 @@ class UpdateProfile(View):
             'email':customer.email
         }
         form = UpdateProfileForm(initial=initial_dict)
-        return render(request,'update_profile.html')
+        return render(request,'update_profile.html',{'form':form})
 
     def post(self,request):
         customer_id = request.session.get('customer')
         customer = Customer.objects.get(id = customer_id)
-        form = UpdateProfileForm(request.POST, instance = customer, initial = customer.data)
+        initial_dict = {
+            'first_name':customer.first_name,
+            'last_name':customer.last_name,
+            'phone':customer.phone,
+            'address':customer.address,
+            'email':customer.email
+        }
+        form = UpdateProfileForm(request.POST, instance = customer, initial = initial_dict)
         if form.is_valid():
             form.save()
-            messages.success('Profile Updated Successfully')
+            messages.success(request,'Profile Updated Successfully')
             return redirect('account:profile')
         else:
-            messages.error('Error in updating profile')
+            messages.error(request,'Error in updating profile')
         context = {'form':form}
         return render(request,'update_profile.html',context)
 
